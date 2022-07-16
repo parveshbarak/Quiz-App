af79c7bba7303845a545b5b536e699b00ea26ec8
Gouri Shanker <shanker.gouri@hotmail.com>
2022-07-16 13:25:37 +0530

    gouri

---
+++ frontend/src/Components/loginModel.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const LoginModel = ({closeLoginForm , handleSubmit}) => {
+	return (
+		<div className="login-form" tabIndex="1" onBlur={closeLoginForm}>
+			<div className="title">Sign In</div>
+			<div className="form">
+				<form onSubmit={handleSubmit}>
+					<div className="input-container">
+						<label>Username </label>
+						<input type="text" name="username" required />
+					</div>
+					<div className="input-container">
+						<label>Password </label>
+						<input type="password" name="password" required />
+					</div>
+					<div className="button-container">
+						<input type="submit" />
+					</div>
+				</form>
+			</div>
+		</div>
+	);
+};
+
+export default LoginModel;
